e4526d8c0e3d699103027c82edfc3f8914dd47e5
Tejashvi <tejashvikumawat@gmail.com>
2025-05-27 13:35:41 +0530

    changes in req

--- startup_platform/startup_platform/urls.py
+++ startup_platform/startup_platform/urls.py
@@ -2,9 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+def home(request):
+    return HttpResponse("You are on wrong place bache")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
     path('api/auth/', include('accounts.urls')),
     path('api/startups/', include('startups.urls')),
 ]
