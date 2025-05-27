40904e177f027fdb3db6d17afef0e1fd9b9b2613
Tejashvi <tejashvikumawat@gmail.com>
2025-05-27 13:30:48 +0530

    changes in req

--- startup_platform/startup_platform/settings.py
+++ startup_platform/startup_platform/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-your-secret-key-here'
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*',]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -83,6 +83,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
